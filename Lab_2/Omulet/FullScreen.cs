b506719c3d5db18d826b77725c51f0801cb2231f
Xhadrines <sandru99alex@gmail.com>
2023-10-24 16:39:32 +0300

    Laborator 2

---
+++ Lab_2/Omulet/FullScreen.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Omulet
+{
+    public class FullScreen
+    {
+        public void GoFullScreen()
+        {
+            Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+
+            Console.SetWindowPosition(0, 0);
+
+            Console.SetBufferSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+        }
+    }
+}
---
+++ Lab_2/Omulet/Lab2.cs
@@ -0,0 +1,32 @@
+﻿/************************************************
+ * Autor: Sandru Alexandru
+ * Data: 23.07.2023
+ * Descriere: Lucrare 2 pentru laboratorul de EGC
+ ************************************************/
+
+using System;
+
+namespace Omulet
+{
+    internal class Lab2
+    {
+        [STAThread]
+        public static void Main()
+        {
+            FullScreen fullScreen = new FullScreen();
+
+            fullScreen.GoFullScreen();
+
+            using (Character start = new Character())
+            {
+                Raspunsurii raspunsurii = new Raspunsurii("Raspunsuri.txt");
+
+                raspunsurii.CitesteFisier();
+
+                Console.WriteLine("\nOptiuni Program:\n" + "\tESC - Inchidere program\n" + "\tA - Invartire la stanga\n" + "\tD - Invartire la dreapta\n" + "\tS - Activare/Dezactivare mouse\n");
+                
+                start.Run(60.0, 60.0);
+            }
+        }
+    }
+}
