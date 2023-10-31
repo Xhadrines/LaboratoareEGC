5ff11599e7cb7d2301a3b39a77b63464d593564e
Xhadrines <sandru99alex@gmail.com>
2023-10-31 09:25:42 +0200

    Recreare Proiecte
    Am recreiat proiectul pentru a adauga un meniu

---
+++ Lab_3/Lab_3.cs
@@ -0,0 +1,21 @@
+﻿/************************************************
+ * Autor: Sandru Alexandru
+ * Data: 23.10.2023
+ * Descriere: Lucrare 3 pentru laboratorul de EGC
+ ************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+    internal class Lab_3
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Lab_Meniu/Lab_Meniu.cs
@@ -0,0 +1,21 @@
+﻿/************************************************
+ * Autor: Sandru Alexandru
+ * Data: 31.10.2023
+ * Descriere: Meniul pentru selectarea fiecarei lucrari de laborator pentru EGC
+ ************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Meniu
+{
+    internal class Lab_Meniu
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
